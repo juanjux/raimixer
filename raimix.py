e1fcc85e2a0bf3c77be88985b0032e7d685c7c91
Juanjo Alvarez <gorthol@protonmail.com>
2017-12-18 13:42:52 +0100

    Moved to a class
    Signed-off-by: gorthol <gorthol@protonmail.com>

--- raimix.py
+++ raimix.py
@@ -1,179 +1,199 @@
+import string
 import random
 from pprint import pprint
 from typing import List, Dict, Optional
 
-ORIG_ACCOUNT = 'ORIG'
-ORIG_BALANCE = 10
-DEST_BALANCE = 8
-DEST_ACCOUNT = 'DEST'
-MIX_ACCOUNTS: List[str] = ['A', 'B', 'C', 'D', 'E']
-NUM_ROUNDS = 3
-FINAL_SENT_FROM_MULTIPLE = True
 
-# TODO: Sent more than needed to the mixing accounts so it's harder
-# to track based on the exact amount received by the destination. This balance
-# would be returned at the end.
+class RaiMixer:
+    def __init__(self, orig_account: str, dest_account: str,
+            dest_balance: int, orig_balance: int, num_mix_accounts=5, num_rounds=4,
+            final_send_from_multiple = False) -> None:
 
+        assert(num_mix_accounts > 1)
+        assert(num_rounds > 0)
 
-def load_balances() -> Dict[str, int]:
-    balances: Dict[str, int] = {}
+        self.orig_account = orig_account
+        self.dest_account = dest_account
+        self.orig_balance = orig_balance
+        self.dest_balance = dest_balance
+        self.num_mix_accounts = num_mix_accounts
+        self.num_rounds = num_rounds
+        self.final_send_from_multiple = final_send_from_multiple
 
-    for acc in MIX_ACCOUNTS:
-        balances[acc] = 0
+        self.mix_accounts: List[str] = []
+        self.balances: Dict[str, int] = {}
+        self.tx_counter = 0
 
-    balances[ORIG_ACCOUNT] = ORIG_BALANCE
-    balances[DEST_ACCOUNT] = 0
+    def start(self) -> None:
+        self.mix_accounts = self._generate_accounts(self.num_mix_accounts)
 
-    return balances
+        self._load_balances()
 
+        # Choose the first accounts to receive funds
+        num_first_dests = random.randrange(2, len(self.mix_accounts) + 1)
+        first_dests = random.choices(self.mix_accounts, k=num_first_dests)
 
-def check_balances(balances: Dict[str, int]) -> None:
-    total = 0
+        print('\nStarting sending to initial mixing accounts...')
+        self._send_one_to_many(self.orig_account, first_dests)
 
-    for k, v in balances.items():
-        total += v
+        # Shake it!
+        for i in range(self.num_rounds):
+            print('\nStarting mixing round %d...' % (i + 1))
+            self._mix()
 
-    assert(total == ORIG_BALANCE)
+        self._send_to_dest()
 
+        print('\nDone! Total transactions done: %d' % self.tx_counter)
+        assert(self.balances[self.orig_account] == self.orig_balance - self.dest_balance)
+        assert(self.balances[self.dest_account] == self.dest_balance)
 
-def send(balances, orig: str, dest: str, amount: int) -> None:
-    orig_balance = balances[orig]
-    dest_balance = balances[dest]
+    def _generate_accounts(self, num: int) -> List[str]:
+        # XXX generate using the RPC
+        res: List[str] = []
 
-    assert(amount <= orig_balance)
+        while len(res) <= num:
+            addr = ''.join(random.choices(string.ascii_lowercase, k=5))
+            if addr in res:
+                continue
+            res.append(addr)
 
-    try:
-        balances[orig] -= amount
-        balances[dest] += amount
-    except:
-        balances[orig] = orig_balance
-        balances[dest] = dest_balance
-        raise
+        return res
 
-    print("Sent {} from {} to {}".format(amount, orig, dest))
-    pprint(balances)
+    def _load_balances(self) -> None:
+        for acc in self.mix_accounts:
+            self.balances[acc] = 0
 
-    check_balances(balances)
-    assert(amount > 0)
+        self.balances[self.orig_account] = self.orig_balance
+        self.balances[self.dest_account] = 0
 
+    def _send_one_to_many(self, from_: str, dests: List[str],
+                          max_send: Optional[int] = None) -> None:
+        # from_ could be in dests. This is not a bug but allows for letting some
+        # amount in the from_ account if the caller want that to happen (like when mixing)
 
-def random_amounts_split(total: int, num_accounts: int) -> List[int]:
-    # loop calculating a random amount from 0 to 1/3 of the remainder until
-    # the pending amount is 1/10, then send that to a random account
-    tosend: Dict[int, int] = {}
-    for a in range(num_accounts):
-        tosend[a] = 0
+        split = self._random_amounts_split(self.balances[from_], len(dests))
 
-    remaining = total
+        for idx, am in enumerate(split):
+            if am > 0 and dests[idx] != from_:
+                self._send(from_, dests[idx], am)
 
-    while True:
-        amount = random.randint(1, max(remaining // num_accounts, 1))
-        dest = random.randrange(0, num_accounts)
-        tosend[dest] += amount
-        remaining -= amount
+    def _send_many_to_one(self, froms: List[str], dest: str,
+                          max_send: Optional[int] = None) -> None:
 
-        if remaining == 0:
-            break
+        already_sent = 0
 
-        if remaining < total / len(MIX_ACCOUNTS):
-            tosend[dest] += remaining
-            break
+        for acc in froms:
+            balance = self.balances[acc]
 
-    return [tosend[i] for i in tosend.keys()]
-
-
-def mix(balances: Dict[str, int]) -> None:
-    for acc, balance in balances.items():
-        if balance > 0:
-            mix_plusorig = MIX_ACCOUNTS.copy()
-            mix_plusorig.append(ORIG_ACCOUNT)
-
-            send_one_to_many(balances, acc, mix_plusorig)
-
-
-def send_to_dest(balances: Dict[str, int]) -> None:
-    remaining_tosend = DEST_BALANCE
-
-    # Move any balance in the orig account into one of the mixer accounts
-    if balances[ORIG_ACCOUNT] > 0:
-        print('\nMoving remaining balance in the orig account to mix accounts...')
-        send_one_to_many(balances, ORIG_ACCOUNT, MIX_ACCOUNTS)
-
-    if not FINAL_SENT_FROM_MULTIPLE:
-        # Choose a single non-origin account to sent from, collect
-        # all the balances to it
-        send_from_acc = random.choice(MIX_ACCOUNTS)
-        print('\nSending to final carrier account [%s]...' % send_from_acc)
-
-        for acc, balance in balances.items():
-            if acc == send_from_acc:
+            if acc == dest or balance == 0:
                 continue
 
+            tosend: int = 0
+            if max_send is not None and (already_sent + balance > max_send):
+                tosend = max_send - already_sent
+            else:
+                tosend = balance
+
+            self._send(acc, dest, tosend)
+            already_sent += tosend
+
+            if max_send is not None:
+                assert(already_sent <= max_send)
+                if already_sent == max_send:
+                    return
+
+    def _send(self, orig: str, dest: str, amount: int) -> None:
+        orig_balance = self.balances[orig]
+        dest_balance = self.balances[dest]
+
+        assert(amount <= orig_balance)
+
+        try:
+            self.balances[orig] -= amount
+            self.balances[dest] += amount
+            self.tx_counter += 1
+        except:
+            self.balances[orig] = orig_balance
+            self.balances[dest] = dest_balance
+            raise
+
+        print("Sent {} from {} to {}".format(amount, orig, dest))
+        pprint(self.balances)
+
+        self._check_balances()
+        assert(amount > 0)
+
+    def _check_balances(self):
+        total = 0
+
+        for k, v in self.balances.items():
+            total += v
+
+        assert(total == self.orig_balance)
+
+    def _mix(self) -> None:
+        for acc, balance in self.balances.items():
             if balance > 0:
-                send(balances, acc, send_from_acc, balance)
+                mix_plusorig = self.mix_accounts.copy()
+                mix_plusorig.append(self.orig_account)
+                self._send_one_to_many(acc, mix_plusorig)
 
-    print('\nSending to destination account...')
-    send_many_to_one(balances, MIX_ACCOUNTS + [ORIG_ACCOUNT], DEST_ACCOUNT, DEST_BALANCE)
+    def _send_to_dest(self) -> None:
+        # Move any balance in the orig account into one of the mixer accounts
+        if self.balances[self.orig_account] > 0:
+            print('\nMoving remaining balance in the orig account to mix accounts...')
+            self._send_one_to_many(self.orig_account, self.mix_accounts)
 
-    # Send the rest back to the orig account
-    print('\nSending remaining balance back to the orig account...')
-    send_many_to_one(balances, MIX_ACCOUNTS, ORIG_ACCOUNT)
+        if not self.final_send_from_multiple:
+            # Choose a single non-origin account to sent from, collect
+            # all the balances to it
+            send_from_acc = random.choice(self.mix_accounts)
+            print('\nSending to final carrier account [%s]...' % send_from_acc)
 
+            for acc, balance in self.balances.items():
+                if acc == send_from_acc:
+                    continue
 
-def send_one_to_many(balances: Dict[str, int], from_: str, dests: List[str]) -> None:
-    # from_ could be in dests. This is not a bug but allows for letting some
-    # amount in the from_ account if the caller want that to happen (like when mixing)
+                if balance > 0:
+                    self._send(acc, send_from_acc, balance)
 
-    split = random_amounts_split(balances[from_], len(dests))
+        print('\nSending to destination account...')
+        self._send_many_to_one(self.mix_accounts + [self.orig_account],
+                               self.dest_account, self.dest_balance)
 
-    for idx, am in enumerate(split):
-        if am > 0 and dests[idx] != from_:
-            send(balances, from_, dests[idx], am)
+        # Send the rest back to the orig account
+        print('\nSending remaining balance back to the orig account...')
+        self._send_many_to_one(self.mix_accounts, self.orig_account)
 
+    def _random_amounts_split(self, total: int, num_accounts: int) -> List[int]:
+        # loop calculating a random amount from 0 to 1/3 of the remainder until
+        # the pending amount is 1/10, then send that to a random account
+        tosend: Dict[int, int] = {}
+        for a in range(num_accounts):
+            tosend[a] = 0
 
-def send_many_to_one(balances: Dict[str, int], froms: List[str], dest: str,
-        max_send: Optional[int] = None) -> None:
+        remaining = total
 
-    already_sent = 0
+        while True:
+            amount = random.randint(1, max(remaining // num_accounts, 1))
+            dest = random.randrange(0, num_accounts)
+            tosend[dest] += amount
+            remaining -= amount
 
-    for acc in froms:
-        balance = balances[acc]
+            if remaining == 0:
+                break
 
-        if acc == dest or balance == 0:
-            continue
+            if remaining < total / len(self.mix_accounts):
+                tosend[dest] += remaining
+                break
 
-        tosend: int = 0
-        if max_send is not None and (already_sent + balance > max_send):
-            tosend = max_send - already_sent
-        else:
-            tosend = balance
-
-        send(balances, acc, dest, tosend)
-        already_sent += tosend
-
-        if max_send is not None:
-            assert(already_sent <= max_send)
-            if already_sent == max_send:
-                return
+        return [tosend[i] for i in tosend.keys()]
 
 
 def main():
-    balances = load_balances()
-
-    # Choose the first accounts to receive funds
-    num_first_dests = random.randrange(2, len(MIX_ACCOUNTS) + 1)
-    first_dests = random.choices(MIX_ACCOUNTS, k=num_first_dests)
-    print('\nStarting sending to initial mixing accounts...')
-    send_one_to_many(balances, ORIG_ACCOUNT, first_dests)
-
-    # Shake it!
-    for i in range(NUM_ROUNDS):
-        print('\nStarting mixing round %d...' % (i + 1))
-        mix(balances)
-
-    send_to_dest(balances)
-    assert(balances[ORIG_ACCOUNT] == ORIG_BALANCE - DEST_BALANCE)
-    assert(balances[DEST_ACCOUNT] == DEST_BALANCE)
+    mixer = RaiMixer('ORIG', 'DEST', 800, 1000, num_mix_accounts=5, num_rounds=2,
+                     final_send_from_multiple = True)
+    mixer.start()
 
 
 if __name__ == '__main__':
